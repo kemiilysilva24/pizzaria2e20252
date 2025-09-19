c5802551e976163eb4596474e5ccd67ded29d205
kemiillysilva24 <rm95211@estudante.fieb.edu.br>
2025-09-19 11:05:50 -0300

    consumo

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,30 +1,15 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react';
-
+ 
 const Produtos = () => {
-    const [dados,setDados] = useState([]);
-    // Consumir os produtos do endpoint (rota)
-    const apiUrl = 'http://172.19.0.49/pizzariaoficial/api/v1'
-    const api = axios.create({baseURL: apiUrl})
-    const pegarProdutos = async () => {
-        await api.get("/produto")
-        .then((response)=>{
-            setDados(response.data);
-        }).catch(err => {
-            console.error("Erro na requisição:", err.message);
-            console.error("Config:", err.config?.url);
-        });   
-    }
-    useEffect (() => {
-        pegarProdutos()
-    }, []);
-    useEffect(()=>{
-        alert(JSON.stringify(dados))
-    }, [dados])
-
+    const [pizzas, setPizzas] = useState([])
+ 
+    axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
+    .then(response=>setPizzas(response.data.data))
+ 
     // Mapeamento das pizzas da lista (iteração)
-    const listaPizzas = dados.map(pizza => 
-        <li key={pizza.id}>{pizza.nome}</li>);
+    const listaPizzas = pizzas.map(pizza =>
+                    <li key={pizza.id}>{pizza.nome}</li>);
     return(
     <>
         <h3>Listagem de Produtos</h3>
